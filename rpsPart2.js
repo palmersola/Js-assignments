55bc5bb9c456c800043f9d07666effa5e06fffb7
palmersola <palmersola@yahoo.com>
2023-04-07 09:50:30 -0500

    rpsPart2

--- rpsPart2.js
+++ rpsPart2.js
@@ -4,17 +4,28 @@ let getHand = function() {
   return hands[parseInt(Math.random() * 10) % 3];
 };
 
-let plyr1 = {
-  name: "Palmer",
-  hand: getHand(),
-  wins: 0
-};
-
-let plyr2 = {
-  name: "Loser",
-  hand: getHand(),
-  wins: 0
-};
+let players = [
+  {
+    name: "Palmer",
+    hand: getHand(),
+    wins: 0
+  },
+  {
+    name: "Jay",
+    hand: getHand(),
+    wins: 0
+  },
+  {
+    name: "Peter",
+    hand: getHand(),
+    wins: 0
+  },
+  {
+    name: "Sarah",
+    hand: getHand(),
+    wins: 0
+  }
+];
 
 let playRound = function(p1, p2) {
   if (p1.hand === p2.hand) {
@@ -58,21 +69,44 @@ let playRound = function(p1, p2) {
       }
     }
   }
-  play();
+  playUntil(p1, p2);
 };
 
-let play = function(p1, p2) {
-  console.log(p1.wins);
+let playUntil = function(p1, p2) {
+  p1.hand = getHand();
   p2.hand = getHand();
-  if (p1.wins === 3) {
-    console.log(p1.name + " wins!");
-  } else if (p2.wins === 3) {
-    console.log(p2.name + " wins!");
-  } else {
-    setTimeout(() => {
-      playRound(p1, p2);
-    }, 1000);
-  }
+  if (p1.wins === 5 || p2.wins === 5) {
+    if (p1.wins === 5) {
+      winner = p1;
+    } else winner = p2;
+    p1.wins = 0;
+    p2.wins = 0;
+    return winner;
+  } else playRound(p1, p2);
 };
 
-play(plyr1, plyr2);
+let playGame = function(p1, p2) {
+  console.log(p1.name + " vs " + p2.name);
+  console.log("");
+  playUntil(p1, p2);
+  console.log("");
+  console.log(winner.name + " wins the round");
+  console.log("");
+};
+
+let playTournament = function(p) {
+  console.log("Round 1");
+  playGame(p[0], p[1]);
+  let game1 = winner;
+
+  console.log("Round 2");
+  playGame(p[2], p[3]);
+  let game2 = winner;
+
+  console.log("Final Round");
+  playGame(game1, game2);
+  let tournamentWinner = winner;
+  console.log(tournamentWinner.name + " is the WORLD CHAMPION!!!");
+};
+
+playTournament(players);
