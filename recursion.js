35cf83350e1fd54d87dc362292c6d6c6c22c2efb
palmersola <palmersola@yahoo.com>
2023-04-07 13:06:44 -0500

    cleaned up recursion

--- recursion.js
+++ recursion.js
@@ -1,6 +1,5 @@
-function recursion(x, y) {
-  if (y === 0) return 1;
-  else return x * recursion(x, y - 1);
-}
+recursion = (x, y) => {
+  return y === 0 ? 1 : x * recursion(x, y - 1);
+};
 
-console.log(recursion(9, 4));
+console.log(recursion(72, 4));
