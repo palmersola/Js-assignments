e207d8d07c535fdbe08aa22e84efe88f33ea38a4
palmersola <palmersola@yahoo.com>
2023-04-26 09:46:59 -0500

    react

---
+++ react-practice/new-app/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import Counter from "./Counter";
+
+export default function App() {
+  return (
+    <div>
+      <h1>React Counter</h1>
+      <Counter />
+    </div>
+  );
+}
---
+++ react-practice/new-app/src/Counter.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "./App.css";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
+  const decrement = () => setCount(count - 1);
+  return (
+    <div>
+      <h2>
+        {count}
+      </h2>
+      <button onClick={() => increment()}>Add to Count</button>
+      <button onClick={() => decrement()}>Subtract from Count</button>
+    </div>
+  );
+}
